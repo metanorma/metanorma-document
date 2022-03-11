d39abe5bf47d81a1347aeedf92fed16e958c5c8d
webdev778 <webdev778@gmail.com>
2022-03-11 23:53:00 +0900

    Initial gem commit

---
+++ lib/metanorma/standoc/document.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "document/version"
+
+module Metanorma
+  module Standoc
+    module Document
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/metanorma/standoc/document_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Metanorma::Standoc::Document do
+  it "has a version number" do
+    expect(Metanorma::Standoc::Document::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
