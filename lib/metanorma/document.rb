17948d033c9650a4c6c5d57a1322c521d81945ef
webdev778 <webdev778@gmail.com>
2022-04-27 15:24:15 +0900

    Expand Metanorma::Document module documentation

--- lib/metanorma/document.rb
+++ lib/metanorma/document.rb
@@ -7,6 +7,19 @@ module Metanorma
   # Metanorma::Document is an abstraction between Nokogiri and Metanorma.
   # It deals with creating a class-based document to be used for handling
   # and converting Metanorma XML documents.
+  #
+  # It represents the documents as a single-linked tree structure, always
+  # starting from a Metanorma::Document::Core::Top. This node responds to
+  # a #root property that corresponds to the top XML element.
+  #
+  # It is intended to replace the current (previous?) system of Metanorma
+  # programs communicating with one another with XML files, but also allow
+  # for a seamless migration from/to those.
+  #
+  # Under the Core module there is a logic to make all this work. Under
+  # the Devel module there are helper modules, to be used for development of
+  # this system. Other modules represent the class systems for the
+  # individual document types.
   module Document
     module_function
 
