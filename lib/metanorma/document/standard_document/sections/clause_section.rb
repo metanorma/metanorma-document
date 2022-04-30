8e412e19ff5efc74c12ed375152108d941f2bf5f
webdev778 <webdev778@gmail.com>
2022-05-01 00:26:38 +0200

    [xml] fixup

--- lib/metanorma/document/standard_document/sections/clause_section.rb
+++ lib/metanorma/document/standard_document/sections/clause_section.rb
@@ -5,7 +5,7 @@ require "metanorma/document/standard_document/sections/standard_section"
 module Metanorma; module Document; module StandardDocument
   # A numbered clause constituting part of the main body of a _StandardDocument_.
   class ClauseSection < StandardSection
-    register_element "Clause-Section" do
+    register_element "clause" do
       # Semantic class of the numbered clause.
       attribute :type, String
     end
