89ab485c68e6157556011542d17110d8c592a003
Ronald Tse <ronald.tse@ribose.com>
2026-05-06 12:47:43 +0800

    chore: update rubocop_todo, remove component_registry, fix spec to avoid stubs

--- spec/metanorma/html/renderer/standard_renderer_spec.rb
+++ spec/metanorma/html/renderer/standard_renderer_spec.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require "metanorma/html/standard_renderer"
+require "metanorma/html/generator"
 
 RSpec.describe Metanorma::Html::StandardRenderer do
-  let(:renderer) { described_class.new }
-
-  def stub_section_renderer(renderer, **extra_stubs)
-    allow(renderer).to receive_messages(
-      safe_attr: nil, element_attrs: "", **extra_stubs,
-    )
-    allow(renderer).to receive(:tag) { |_tag, _attrs, &blk| blk.call }
-    allow(renderer).to receive(:render_standard_title)
-    allow(renderer).to receive(:render_standard_section_blocks)
-    allow(renderer).to receive(:render_subsections)
-  end
+  let(:xml_path) { File.expand_path("../../../fixtures/iso/is/document-en.presentation.xml", __dir__) }
+  let(:xml) { File.read(xml_path) }
+  let(:doc) { Metanorma::IsoDocument::Root.from_xml(xml) }
+  let(:html) { Metanorma::Html::Generator.generate(doc) }
+  let(:page) { Nokogiri::HTML(html) }
 
   describe "#render_section" do
-    it "delegates clause sections with_subsections" do
-      stub_section_renderer(renderer)
-      renderer.should receive(:render_section).with(anything, hash_including(with_subsections: true))
-      renderer.send(:render_clause_section, double)
+    it "renders clause sections with nested headings" do
+      headings = page.css("main div[id] h2, main div[id] h3")
+      headings.length.should be > 0
     end
 
-    it "delegates terms sections with_terms" do
-      section = double(terms: [])
-      stub_section_renderer(renderer)
-      renderer.should receive(:render_section).with(anything, hash_including(with_terms: true))
-      renderer.send(:render_terms_section, section)
+    it "renders terms sections with term-number entries" do
+      page.at_css(".term-number").should_not be_nil
     end
 
-    it "delegates foreword with title_class" do
-      stub_section_renderer(renderer)
-      renderer.should receive(:render_section).with(anything, hash_including(title_class: "foreword-title"))
-      renderer.send(:render_foreword_section, double)
+    it "renders foreword with foreword-title class" do
+      page.at_css(".foreword-title").should_not be_nil
+    end
+
+    it "does not emit raw XML class names in sections" do
+      xml_classes = %w[ForewordTitle IntroTitle Section3 Annex]
+      all_classes = page.css("[class]").flat_map { |el| el["class"].split(/\s+/) }.uniq
+      (all_classes & xml_classes).should be_empty
+    end
+
+    it "renders all main sections inside main element" do
+      main = page.at_css("main")
+      main.should_not be_nil
+      main.inner_html.length.should be > 100
     end
   end
 end
