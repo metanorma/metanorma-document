4aef77b68134a21405f2ca1a23fd63c30072c8b8
Ronald Tse <ronald.tse@ribose.com>
2026-06-02 06:52:57 +0800

    fix: add Home/End keyboard shortcuts for document navigation

--- lib/metanorma/html/bipm_renderer.rb
+++ lib/metanorma/html/bipm_renderer.rb
@@ -4,13 +4,20 @@ module Metanorma
   module Html
     class BipmRenderer < IsoRenderer
       register_render Metanorma::BipmDocument::Root, :render_document
-      register_render Metanorma::StandardDocument::Sections::Preface, :render_preface
-      register_render Metanorma::StandardDocument::Sections::ClauseSection, :render_clause
-      register_render Metanorma::StandardDocument::Sections::AnnexSection, :render_annex
-      register_render Metanorma::StandardDocument::Sections::ContentSection, :render_clause
-      register_render Metanorma::StandardDocument::Sections::TermsSection, :render_terms_section
-      register_render Metanorma::StandardDocument::Sections::BibliographySection, :render_clause
-      register_render Metanorma::StandardDocument::Sections::DefinitionSection, :render_clause
+      register_render Metanorma::StandardDocument::Sections::Preface,
+                      :render_preface
+      register_render Metanorma::StandardDocument::Sections::ClauseSection,
+                      :render_clause
+      register_render Metanorma::StandardDocument::Sections::AnnexSection,
+                      :render_annex
+      register_render Metanorma::StandardDocument::Sections::ContentSection,
+                      :render_clause
+      register_render Metanorma::StandardDocument::Sections::TermsSection,
+                      :render_terms_section
+      register_render Metanorma::StandardDocument::Sections::BibliographySection,
+                      :render_clause
+      register_render Metanorma::StandardDocument::Sections::DefinitionSection,
+                      :render_clause
     end
   end
 end
