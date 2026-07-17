9b92b593af1c1c297268a368d8d360c50d892ee6
Ronald Tse <ronald.tse@ribose.com>
2026-07-17 19:44:30 +0800

    fix(components): FmtStemElement declares mixed_content mode
    FmtStemElement mapped <semx> as a child but did not declare
    mixed_content in its xml do block. As a result, each_mixed_content
    (inherited from Serializable) operated on a non-existent mapping
    and yielded nothing, even though the semx attribute was populated
    (BUGS.sts 03).
    
    Add mixed_content to the xml block. Also adds the RenderedDisplay
    marker module (used by the bug 05 SemanticContent iterator) and a
    text attribute for direct text capture.
    
    Fixes: BUGS.sts 03

--- lib/metanorma/document/components/inline/fmt_stem_element.rb
+++ lib/metanorma/document/components/inline/fmt_stem_element.rb
@@ -4,13 +4,21 @@ module Metanorma
   module Document
     module Components
       module Inline
+        # Rendered counterpart of `<stem>`. Produced by presentation XML
+        # transformation. Wraps a `<semx>` carrying the locale-rendered
+        # form of the math (e.g. decimal commas, thousands separators).
         class FmtStemElement < Lutaml::Model::Serializable
+          include RenderedDisplay
+
           attribute :stem_type, :string
+          attribute :text, :string, collection: true
           attribute :semx, SemxElement, collection: true
 
           xml do
             element "fmt-stem"
+            mixed_content
             map_attribute "type", to: :stem_type
+            map_content to: :text
             map_element "semx", to: :semx
           end
         end
---
+++ spec/metanorma/document/components/inline/fmt_stem_element_bug03_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "metanorma/document"
+
+RSpec.describe "BUGS.sts 03: FmtStemElement declares mixed_content" do
+  it "each_mixed_content yields its semx children" do
+    fmt_stem = Metanorma::Document::Components::Inline::FmtStemElement.from_xml(<<~XML)
+      <fmt-stem xmlns="https://www.metanorma.org/ns/standoc" type="MathML">
+        <semx element="stem" source="s1">
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <mn>0,7</mn>
+          </math>
+          <asciimath>0.7</asciimath>
+        </semx>
+      </fmt-stem>
+    XML
+
+    yielded = []
+    fmt_stem.each_mixed_content { |n| yielded << n }
+
+    yielded.any?(Metanorma::Document::Components::Inline::SemxElement).should be(true)
+  end
+
+  it "still exposes semx via the typed attribute" do
+    fmt_stem = Metanorma::Document::Components::Inline::FmtStemElement.from_xml(<<~XML)
+      <fmt-stem xmlns="https://www.metanorma.org/ns/standoc" type="MathML">
+        <semx element="stem" source="s1">
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <mn>0,7</mn>
+          </math>
+        </semx>
+      </fmt-stem>
+    XML
+    Array(fmt_stem.semx).size.should eq(1)
+  end
+end
