f90d5845e15c7824a44bc2ce4abe0aee8048ec7d
Ronald Tse <ronald.tse@ribose.com>
2026-07-18 15:42:44 +0800

    style: apply rubocop -A and regenerate todo baseline

--- lib/metanorma/html/generator.rb
+++ lib/metanorma/html/generator.rb
@@ -43,7 +43,8 @@ module Metanorma
         def find_taste(document)
           @tastes.each do |model_class, publisher_abbrev, renderer_class|
             next unless document.is_a?(model_class)
-            return renderer_class if taste_publisher?(document, publisher_abbrev)
+            return renderer_class if taste_publisher?(document,
+                                                      publisher_abbrev)
           end
           nil
         end
@@ -99,7 +100,8 @@ module Metanorma
 
           # Register tastes (publisher-based dispatch within same model)
           register_taste Metanorma::IsoDocument::Root, "ICC", IccRenderer
-          register_taste Metanorma::RiboseDocument::Root, "PDF Association", PdfaRenderer
+          register_taste Metanorma::RiboseDocument::Root, "PDF Association",
+                         PdfaRenderer
         end
 
         # Single source of truth for flavor identity. Each Flavor ties
@@ -109,81 +111,81 @@ module Metanorma
         def build_flavor_registry
           FlavorRegistry.new.tap do |registry|
             registry.register(Flavor.new(
-              name: nil,
-              model_class: Metanorma::Document::Root,
-              renderer_class: BaseRenderer,
-            ))
+                                name: nil,
+                                model_class: Metanorma::Document::Root,
+                                renderer_class: BaseRenderer,
+                              ))
             registry.register(Flavor.new(
-              name: nil,
-              model_class: Metanorma::StandardDocument::Root,
-              renderer_class: StandardRenderer,
-            ))
+                                name: nil,
+                                model_class: Metanorma::StandardDocument::Root,
+                                renderer_class: StandardRenderer,
+                              ))
             registry.register(Flavor.new(
-              name: :iso,
-              model_class: Metanorma::IsoDocument::Root,
-              renderer_class: IsoRenderer,
-              pubid_module: :"Pubid::Iso",
-            ))
+                                name: :iso,
+                                model_class: Metanorma::IsoDocument::Root,
+                                renderer_class: IsoRenderer,
+                                pubid_module: :"Pubid::Iso",
+                              ))
             registry.register(Flavor.new(
-              name: :bipm,
-              model_class: Metanorma::BipmDocument::Root,
-              renderer_class: BipmRenderer,
-            ))
+                                name: :bipm,
+                                model_class: Metanorma::BipmDocument::Root,
+                                renderer_class: BipmRenderer,
+                              ))
             registry.register(Flavor.new(
-              name: :cc,
-              model_class: Metanorma::CcDocument::Root,
-              renderer_class: CcRenderer,
-            ))
+                                name: :cc,
+                                model_class: Metanorma::CcDocument::Root,
+                                renderer_class: CcRenderer,
+                              ))
             registry.register(Flavor.new(
-              name: :iec,
-              model_class: Metanorma::IecDocument::Root,
-              renderer_class: IecRenderer,
-              pubid_module: :"Pubid::Iec",
-            ))
+                                name: :iec,
+                                model_class: Metanorma::IecDocument::Root,
+                                renderer_class: IecRenderer,
+                                pubid_module: :"Pubid::Iec",
+                              ))
             registry.register(Flavor.new(
-              name: :ieee,
-              model_class: Metanorma::IeeeDocument::Root,
-              renderer_class: IeeeRenderer,
-              pubid_module: :"Pubid::Ieee",
-            ))
+                                name: :ieee,
+                                model_class: Metanorma::IeeeDocument::Root,
+                                renderer_class: IeeeRenderer,
+                                pubid_module: :"Pubid::Ieee",
+                              ))
             registry.register(Flavor.new(
-              name: :ietf,
-              model_class: Metanorma::IetfDocument::Root,
-              renderer_class: IetfRenderer,
-            ))
+                                name: :ietf,
+                                model_class: Metanorma::IetfDocument::Root,
+                                renderer_class: IetfRenderer,
+                              ))
             registry.register(Flavor.new(
-              name: :iho,
-              model_class: Metanorma::IhoDocument::Root,
-              renderer_class: IhoRenderer,
-              pubid_module: :"Pubid::Iho",
-            ))
+                                name: :iho,
+                                model_class: Metanorma::IhoDocument::Root,
+                                renderer_class: IhoRenderer,
+                                pubid_module: :"Pubid::Iho",
+                              ))
             registry.register(Flavor.new(
-              name: :itu,
-              model_class: Metanorma::ItuDocument::Root,
-              renderer_class: ItuRenderer,
-              pubid_module: :"Pubid::Ithu",
-            ))
+                                name: :itu,
+                                model_class: Metanorma::ItuDocument::Root,
+                                renderer_class: ItuRenderer,
+                                pubid_module: :"Pubid::Ithu",
+                              ))
             registry.register(Flavor.new(
-              name: :ogc,
-              model_class: Metanorma::OgcDocument::Root,
-              renderer_class: OgcRenderer,
-            ))
+                                name: :ogc,
+                                model_class: Metanorma::OgcDocument::Root,
+                                renderer_class: OgcRenderer,
+                              ))
             registry.register(Flavor.new(
-              name: :oiml,
-              model_class: Metanorma::OimlDocument::Root,
-              renderer_class: OimlRenderer,
-              pubid_module: :"Pubid::Oiml",
-            ))
+                                name: :oiml,
+                                model_class: Metanorma::OimlDocument::Root,
+                                renderer_class: OimlRenderer,
+                                pubid_module: :"Pubid::Oiml",
+                              ))
             registry.register(Flavor.new(
-              name: :pdfa,
-              model_class: Metanorma::RiboseDocument::Root,
-              renderer_class: PdfaRenderer,
-            ))
+                                name: :pdfa,
+                                model_class: Metanorma::RiboseDocument::Root,
+                                renderer_class: PdfaRenderer,
+                              ))
             registry.register(Flavor.new(
-              name: :ribose,
-              model_class: Metanorma::RiboseDocument::Root,
-              renderer_class: RiboseRenderer,
-            ))
+                                name: :ribose,
+                                model_class: Metanorma::RiboseDocument::Root,
+                                renderer_class: RiboseRenderer,
+                              ))
           end
         end
 
